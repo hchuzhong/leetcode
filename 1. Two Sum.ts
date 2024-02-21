a290279962739a542f8d3e1eb369a18397b4dbed
hchuzhong <hchuzhong@163.com>
2024-02-21 21:57:44 +0800

    test git pull

--- 1. Two Sum.ts
+++ 1. Two Sum.ts
@@ -4,7 +4,7 @@ function twoSum(nums: number[], target: number): number[] {
         const curNum = nums[i];
         const subTarget = target - curNum;
         if (map[subTarget] || map[subTarget] === 0) return [map[subTarget], i];
-        map[curNum] = i;
+        map[curNum] = i; 
     }
     return [];
 };
