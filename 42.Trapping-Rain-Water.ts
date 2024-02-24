b5bdbe795142ebf77fca9670e656b1390e0abbae
hchuzhong <hchuzhong@163.com>
2024-02-24 10:53:48 +0800

    Learn the solutions from the comment section

--- 42.Trapping-Rain-Water.ts
+++ 42.Trapping-Rain-Water.ts
@@ -1,5 +1,5 @@
 // Find the two highest values in a range, and you need to subtract the size of the middle block
-function trap(height: number[]): number {
+function trap1(height: number[]): number {
     const length = height.length;
     let result = 0;
     if (length < 3) return result;
@@ -38,6 +38,44 @@ function trap(height: number[]): number {
             continue;
         }
         left++;
+    }
+    return result;
+};
+
+function trap2(height: number[]): number {
+    let result = 0;
+    const length = height.length;
+    const maxLeftArr = new Array(length), maxRightArr = new Array(length);
+    maxLeftArr[0] = height[0];
+    maxRightArr[length - 1] = height[length - 1];
+    for (let i = 1; i < length - 1; i++) {
+        maxLeftArr[i] = Math.max(maxLeftArr[i - 1], height[i - 1]);
+    }
+    for (let i = length - 2; i >= 0; i--) {
+        maxRightArr[i] = Math.max(maxRightArr[i + 1], height[i + 1]);
+    }
+    for (let i = 1; i < length; i++) {
+        const min = Math.min(maxLeftArr[i], maxRightArr[i])
+        if (min > height[i]) result += (min - height[i]);
+    }
+    return result;
+};
+
+function trap3(height: number[]): number {
+    let result = 0;
+    const length = height.length;
+    let maxLeft = 0, maxRight = 0;
+    let left = 1, right = length - 2;
+    for (let i = 1; i < length - 1; i++) {
+        if (height[left - 1] < height[right + 1]) {
+            maxLeft = Math.max(maxLeft, height[left - 1])
+            if (maxLeft > height[left]) result += (maxLeft - height[left])
+            left++;
+        } else {
+            maxRight = Math.max(maxRight, height[right + 1]);
+            if (maxRight > height[right]) result += (maxRight - height[right]);
+            right--;
+        }
     }
     return result;
 };
