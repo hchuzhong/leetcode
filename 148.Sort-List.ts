17845ae66e1df9727b079c1a93d54e79e5c93a55
hchuzhong <hchuzhong@163.com>
2024-03-01 14:18:14 +0800

    add another version

--- 148.Sort-List.ts
+++ 148.Sort-List.ts
@@ -27,3 +27,30 @@ function sortList(head: ListNode | null): ListNode | null {
     }
     return pre.next;
 };
+
+function sortList2(head: ListNode | null): ListNode | null {
+    if (!head || !head.next) return head;
+    let fast = head.next, slow = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    let temp = slow.next;
+    slow.next = null;
+    let left = sortList(head);
+    let right = sortList(temp);
+    let h = new ListNode();
+    let res = h;
+    while (left && right) {
+        if (left.val < right.val) {
+            h.next = left;
+            left = left.next;
+        } else {
+            h.next = right;
+            right = right.next;
+        }
+        h = h.next;
+    }
+    h.next = left ? left : right;
+    return res.next;
+};
