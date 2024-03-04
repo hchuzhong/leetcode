4c8adc536395be819d89f269ce265445d690b14a
hchuzhong <hchuzhong@163.com>
2024-03-04 13:23:22 +0800

    138.Copy-List-with-Random-Pointer

--- 138.Copy-List-with-Random-Pointer.ts
+++ 138.Copy-List-with-Random-Pointer.ts
@@ -12,7 +12,7 @@
  * }
  */
 
-function copyRandomList(head: Node | null): Node | null {
+function copyRandomList1(head: Node | null): Node | null {
     if (!head) return null;
     let result = new Node(head.val);
     let resultHead = result;
@@ -37,4 +37,50 @@ function copyRandomList(head: Node | null): Node | null {
         resultHead = resultHead.next;
     }
     return result;
+};
+
+function copyRandomList2(head: Node | null): Node | null {
+    if (!head) return null;
+    let cur = head;
+    const map = new Map();
+    map.set(null, null);
+    while (cur) {
+        map.set(cur, new Node(cur.val));
+        cur = cur.next;
+    }
+    cur = head;
+    while (cur) {
+        map.get(cur).next = map.get(cur.next);
+        map.get(cur).random = map.get(cur.random);
+        cur = cur.next;
+    }
+    return map.get(head);
+};
+
+function copyRandomList3(head: Node | null): Node | null {
+    if (!head) return null;
+    let cur = head;
+    const map = new Map();
+    map.set(null, null);
+    while (cur) {
+        let temp = new Node(cur.val);
+        temp.next = cur.next;
+        cur.next = temp;
+        cur = temp.next;
+    }
+    cur = head;
+    while (cur) {
+        if (cur.random) cur.next.random = cur.random.next;
+        cur = cur.next.next;
+    }
+    cur = head.next;
+    let pre = head, res = head.next;
+    while (cur.next) {
+        pre.next = pre.next.next;
+        cur.next = cur.next.next;
+        pre = pre.next;
+        cur = cur.next;
+    }
+    pre.next = null;
+    return res;
 };
