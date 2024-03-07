09eabac1bb4e14cfef9587c5d40d8a7f6f300121
hchuzhong <hchuzhong@163.com>
2024-03-07 13:16:21 +0800

    53.Maximum-Subarray

--- 53.Maximum-Subarray.ts
+++ 53.Maximum-Subarray.ts
@@ -1,4 +1,4 @@
-function maxSubArray(nums: number[]): number {
+function maxSubArray1(nums: number[]): number {
     if (nums.length === 1) return nums[0];
     const length = nums.length;
     let dp = new Array(length).fill(Number.NEGATIVE_INFINITY);
@@ -6,6 +6,18 @@ function maxSubArray(nums: number[]): number {
     for (let i = 1; i < length; i++) {
         dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
         res = Math.max(dp[i], res);
+    }
+    return res;
+};
+
+function maxSubArray2(nums: number[]): number {
+    if (nums.length === 1) return nums[0];
+    const length = nums.length;
+    let res = nums[0];
+    let pre = nums[0];
+    for (let i = 1; i < length; i++) {
+        pre = Math.max(pre + nums[i], nums[i]);
+        res = Math.max(pre, res);
     }
     return res;
 };
