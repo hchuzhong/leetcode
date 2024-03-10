fbee390e006ec19087f8febd1027ada930a31c61
hchuzhong <hchuzhong@163.com>
2024-03-10 17:01:29 +0800

    73.Set-Matrix-Zeroes

--- 73.Set-Matrix-Zeroes.ts
+++ 73.Set-Matrix-Zeroes.ts
@@ -1,7 +1,7 @@
 /**
     Do not return anything, modify matrix in-place instead.
  */
-function setZeroes(matrix: number[][]): void {
+function setZeroes1(matrix: number[][]): void {
     let iArr: number[] = [];
     let jArr: number[] = [];
     const matrixLength = matrix.length;
@@ -22,6 +22,26 @@ function setZeroes(matrix: number[][]): void {
     for (let i = 0; i < matrixLength; i++) {
         for (let j = 0; j < jArr.length; j++) {
             matrix[i][jArr[j]] = 0;
+        }
+    }
+};
+
+function setZeroes2(matrix: number[][]): void {
+    const iMap = new Map();
+    const jMap = new Map();
+    const matrixLength = matrix.length;
+    const innerLength = matrix[0].length;
+    for (let i = 0; i < matrixLength; i++) {
+        for (let j = 0; j < innerLength; j++) {
+            if (matrix[i][j] === 0) {
+                iMap.set(i, true);
+                jMap.set(j, true);
+            }
+        }
+    }
+    for (let i = 0; i < matrixLength; i++) {
+        for (let j = 0; j < innerLength; j++) {
+            if (iMap.has(i) || jMap.has(j)) matrix[i][j] = 0;
         }
     }
 };
