d6df67b8cad9d5a1939616dacef7b092229335cb
hchuzhong <hchuzhong@163.com>
2024-03-10 17:15:14 +0800

    73.Set-Matrix-Zeroes

--- 73.Set-Matrix-Zeroes.ts
+++ 73.Set-Matrix-Zeroes.ts
@@ -45,3 +45,45 @@ function setZeroes2(matrix: number[][]): void {
         }
     }
 };
+
+function setZeroes3(matrix: number[][]): void {
+    const m = matrix.length, n = matrix[0].length;
+    let mFlag = false, nFlag = false;
+    for (let i = 0; i < m; i++) {
+        if (matrix[i][0] === 0) {
+            mFlag = true;
+            break;
+        }
+    }
+    for (let j = 0; j < n; j++) {
+        if (matrix[0][j] === 0) {
+            nFlag = true;
+            break;
+        }
+    }
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    if (mFlag) {
+        for (let i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+    if (nFlag) {
+        for (let j = 0; j < n; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+};
