28ec258d21b5d40a94ec9a2b750b8d5c65a241bf
hchuzhong <hchuzhong@163.com>
2024-03-10 16:41:49 +0800

    41.First-Missing-Positive

--- 41.First-Missing-Positive.ts
+++ 41.First-Missing-Positive.ts
@@ -1,8 +1,23 @@
-function firstMissingPositive(nums: number[]): number {
+function firstMissingPositive1(nums: number[]): number {
     const map = {};
     nums.forEach(num => map[num] = true);
     for (let i = 1; i <= nums.length; i++) {
         if (!map[i]) return i;
     }
     return nums.length + 1;
+};
+
+function firstMissingPositive2(nums: number[]): number {
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+            const temp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = temp;
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) return i + 1;
+    }
+    return n + 1;
 };
