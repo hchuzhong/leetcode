4b890722020cf5bdaa01ea822df69ab5bc99e23e
hchuzhong <hchuzhong@163.com>
2024-03-12 22:23:28 +0800

    240.Search-a-2D-Matrix-II

--- 240.Search-a-2D-Matrix-II.ts
+++ 240.Search-a-2D-Matrix-II.ts
@@ -1,4 +1,4 @@
-function searchMatrix(matrix: number[][], target: number): boolean {
+function searchMatrix1(matrix: number[][], target: number): boolean {
     let result = false;
     const m = matrix.length, n = matrix[0].length;
     for (let i = 0; i < m; i++) {
@@ -8,4 +8,15 @@ function searchMatrix(matrix: number[][], target: number): boolean {
         }
     }
     return result;
+};
+
+function searchMatrix2(matrix: number[][], target: number): boolean {
+    const m = matrix.length, n = matrix[0].length;
+    let i = 0, j = n - 1;
+    while (i < m && j >= 0) {
+        if (matrix[i][j] > target) j--;
+        else if (matrix[i][j] < target) i++;
+        else return true;
+    }
+    return false;
 };
