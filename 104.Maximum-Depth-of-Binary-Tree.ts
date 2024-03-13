eec0c193b47da3bc8e6ae95fcff5698a6780e041
hchuzhong <hchuzhong@163.com>
2024-03-13 12:48:46 +0800

    104.Maximum-Depth-of-Binary-Tree

--- 104.Maximum-Depth-of-Binary-Tree.ts
+++ 104.Maximum-Depth-of-Binary-Tree.ts
@@ -1,18 +1,33 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
 
-function maxDepth(root: TreeNode | null): number {
+function maxDepth1(root: TreeNode | null): number {
     if (!root) return 0;
-    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    return Math.max(maxDepth1(root.left), maxDepth1(root.right)) + 1;
+};
+
+function maxDepth2(root: TreeNode | null): number {
+    if (!root) return 0;
+    let queue: TreeNode[] = [];
+    queue.push(root);
+    let result = 0;
+    while (queue.length) {
+        const temp: TreeNode[] = [];
+        for (let i = 0; i < queue.length; i++) {
+            const curNode = queue[i];
+            if (curNode.left) temp.push(curNode.left);
+            if (curNode.right) temp.push(curNode.right);
+        }
+        queue = temp;
+        result++;
+    }
+    return result;
 };
