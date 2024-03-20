8a27d1b62c3566404123552f64d091e4d28b9ab2
hchuzhong <hchuzhong@163.com>
2024-03-20 16:14:20 +0800

    208. Implement Trie (Prefix Tree)

--- 208.Implement-Trie-(Prefix-Tree).ts
+++ 208.Implement-Trie-(Prefix-Tree).ts
@@ -1,4 +1,4 @@
-class Trie {
+class Trie1 {
     arr: string[];
     constructor() {
         this.arr = [];
@@ -38,3 +38,55 @@ class Trie {
  * var param_2 = obj.search(word)
  * var param_3 = obj.startsWith(prefix)
  */
+
+class TireNode {
+    isEnd: boolean;
+    next: TireNode[];
+    
+    constructor() {
+        this.isEnd = false;
+        this.next = new Array(26);
+    }
+}
+
+class Trie2 {
+    root: TireNode;
+    constructor() {
+        this.root = new TireNode();
+    }
+
+    insert(word: string): void {
+        let node = this.root;
+        for (let i = 0; i < word.length; i++) {
+            const aCode = 'a'.charCodeAt(0);
+            const curCode = word[i].charCodeAt(0);
+            if (!node.next[curCode - aCode]) {
+                node.next[curCode - aCode] = new TireNode();
+            }
+            node = node.next[curCode - aCode];
+        }
+        node.isEnd = true;
+    }
+
+    search(word: string): boolean {
+        let node = this.root;
+        for (let i = 0; i < word.length; i++) {
+            const aCode = 'a'.charCodeAt(0);
+            const curCode = word[i].charCodeAt(0);
+            node = node.next[curCode - aCode];
+            if (!node) return false;
+        }
+        return node.isEnd;
+    }
+
+    startsWith(prefix: string): boolean {
+        let node = this.root;
+        for (let i = 0; i < prefix.length; i++) {
+            const aCode = 'a'.charCodeAt(0);
+            const curCode = prefix[i].charCodeAt(0);
+            node = node.next[curCode - aCode];
+            if (!node) return false;
+        }
+        return true;
+    }
+}
