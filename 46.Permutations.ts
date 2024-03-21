5a03996f2cf2fb27322c8e243c3dcf869de00a61
hchuzhong <hchuzhong@163.com>
2024-03-21 15:01:50 +0800

    46.Permutations

--- 46.Permutations.ts
+++ 46.Permutations.ts
@@ -1,4 +1,4 @@
-function permute(nums: number[]): number[][] {
+function permute1(nums: number[]): number[][] {
     if (nums.length === 0) return [];
     const result: number[][] = [];
     let backtrack = (track: number[]) => {
@@ -10,6 +10,29 @@ function permute(nums: number[]): number[][] {
             if (track.includes(num)) continue;
             track.push(num);
             backtrack(track);
+            track.pop();
+        }
+    }
+    backtrack([]);
+    return result;
+};
+
+function permute2(nums: number[]): number[][] {
+    if (nums.length === 0) return [];
+    const result: number[][] = [];
+    const mark = {};
+    nums.forEach(num => mark[num] = false);
+    let backtrack = (track: number[]) => {
+        if (track.length === nums.length) {
+            result.push([...track]);
+            return;
+        }
+        for (const num of nums) {
+            if (mark[num]) continue;
+            track.push(num);
+            mark[num] = true;
+            backtrack(track);
+            mark[num] = false;
             track.pop();
         }
     }
