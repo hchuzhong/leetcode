6e5ea08f78588ec6b74047de774ff79a595c4499
hchuzhong <hchuzhong@163.com>
2024-03-23 14:21:39 +0800

    39. Combination Sum

--- 39.Combination-Sum.ts
+++ 39.Combination-Sum.ts
@@ -1,4 +1,4 @@
-function combinationSum(candidates: number[], target: number): number[][] {
+function combinationSum1(candidates: number[], target: number): number[][] {
     let result: number[][] = [];
     const length = candidates.length;
     if (!length) return result;
@@ -6,6 +6,24 @@ function combinationSum(candidates: number[], target: number): number[][] {
         if (selfTarget < 0) return;
         if (selfTarget === 0) return result.push([...track]);
         for (let i = begin; i < length; i++) {
+            track.push(candidates[i]);
+            backtrack(track, i, selfTarget - candidates[i]);
+            track.pop();
+        }
+    }
+    backtrack([], 0, target);
+    return result;
+};
+
+function combinationSum2(candidates: number[], target: number): number[][] {
+    let result: number[][] = [];
+    const length = candidates.length;
+    if (!length) return result;
+    candidates = candidates.sort((a, b) => a - b);
+    const backtrack = (track: number[], begin: number, selfTarget: number) => {
+        if (selfTarget === 0) return result.push([...track]);
+        for (let i = begin; i < length; i++) {
+            if (selfTarget - candidates[i] < 0) break;
             track.push(candidates[i]);
             backtrack(track, i, selfTarget - candidates[i]);
             track.pop();
