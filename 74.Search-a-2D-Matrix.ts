cc8ebe0333a2255e60d0fdabfd1ec744b9fd059f
hchuzhong <hchuzhong@163.com>
2024-03-28 20:37:07 +0800

    74. Search a 2D Matrix

---
+++ 74.Search-a-2D-Matrix.ts
@@ -0,0 +1,8 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] === target) return true;
+        }
+    }
+    return false;
+};
