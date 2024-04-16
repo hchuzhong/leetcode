55028710c7acd18ab4f159ab62fe243545e45256
hchuzhong <hchuzhong@163.com>
2024-04-16 18:59:16 +0800

    70. Climbing Stairs

--- 70.Climbing-Stairs.ts
+++ 70.Climbing-Stairs.ts
@@ -1,6 +1,16 @@
 const map = {1: 1, 0: 1};
-function climbStairs(n: number): number {
+function climbStairs1(n: number): number {
     if (map[n]) return map[n];
-    map[n] = climbStairs(n - 1) + climbStairs(n - 2);
+    map[n] = climbStairs1(n - 1) + climbStairs1(n - 2);
     return map[n];
+};
+
+function climbStairs2(n: number): number {
+    let a = 1, b = 1, sum = 0;
+    for (let i = 0; i < n - 1; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    return b;
 };
