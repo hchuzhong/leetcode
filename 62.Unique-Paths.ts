81baf3437a58c5f2605cd8d72dbf4c60db0bd867
hchuzhong <hchuzhong@163.com>
2024-04-29 17:03:52 +0800

    62. Unique Paths

---
+++ 62.Unique-Paths.ts
@@ -0,0 +1,12 @@
+function uniquePaths1(m: number, n: number): number {
+    const tempArr = new Array(n).fill(0);
+    const dp = new Array(m).fill([...tempArr]);
+    for (let i = 0; i < n; i++) dp[0][i] = 1;
+    for (let i = 0; i < m; i++) dp[i][0] = 1;
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+    return dp[m - 1][n - 1];
+};
