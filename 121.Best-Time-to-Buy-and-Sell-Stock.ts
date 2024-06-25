ba4119f6c6eb7a5b5c10626c6dc8c50d9b5fb66c
hchuzhong <hchuzhong@163.com>
2024-06-25 15:51:02 +0800

    remove old ts file

--- 121.Best-Time-to-Buy-and-Sell-Stock.ts
+++
@@ -1,8 +0,0 @@
-function maxProfit(prices: number[]): number {
-    let cost = prices[0], profit = 0;
-    for (const price of prices) {
-        cost = Math.min(cost, price);
-        profit = Math.max(profit, price - cost);
-    }
-    return profit;
-};
--- 49.Group-Anagrams.ts
+++
@@ -1,9 +0,0 @@
-function groupAnagrams(strs: string[]): string[][] {
-    const map = {};
-    strs.forEach(str => {
-        const sortResult = str.split('').sort().join('');
-        if (!map[sortResult]) map[sortResult] = [];
-        map[sortResult].push(str);
-    })
-    return Object.values(map);
-};
